cc34e06afe1b079302a7804fd06e51f69ee28a8b
Yazdan Ali Khan <yazdanalikhan005@gmail.com>
2026-02-23 00:14:04 +0500

    Delete Sorting_Algorithms.h

--- Sorting_Algorithms.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-void selectionSort(std::vector<float>& arr);
-void bubbleSort(std::vector<float>& arr);
-void insertionSort(std::vector<float>& arr);
-void mergeSort(std::vector<float>& arr); // stable O(n log n)
-void quickSort(std::vector<float>& arr); // average O(n log n)
